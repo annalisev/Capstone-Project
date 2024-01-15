3ae3394d03699a41e3b734a8efcdffa1390b21b0
annalisev <149485371+annalisev@users.noreply.github.com>
2024-01-15 17:38:50 +0000

    Update win_checker.py
    trying with a try except clause which should allow code to continue even if some indexes are out of bounds

--- win_checker.py
+++ win_checker.py
@@ -7,31 +7,20 @@ board = [['0', '0', '0', '0', '0', '0', '0'],
          ['X', '0', '0', 'X', 'X', 'X', 'X']]
 
 
-def win_checker(row, col):
-    if row <= 2:
-        if col >= 3:
-            if board[row][col] == board[row+1][col-1] and board[row][col] == board[row+2][col-2] and board[row][col] == board[row+3][col-3]:
-                return True
-            elif board[row][col] == board[row+1][col] and board[row][col] == board[row+2][col] and board[row][col] == board[row+3][col]:
-                return True
-        if col <= 3:
-            if board[row][col] == board[row+1][col+1] and board[row][col] == board[row+2][col+2] and board[row][col] == board[row+3][col+3]:
-                return True
-        elif board[row][col] == board[row+1][col] and board[row][col] == board[row+2][col] and board[row][col] == board[row+3][col]:
-            return True
-
-    elif col >= 3:
-        if board[row][col] == board[row][col-1] and board[row][col] == board[row][col-2] and board[row][col] == board[row][col-3]:
-            return True
-
-    elif col <= 3:
-        print('here1')
-        if board[row][col] == board[row][col+1] and board[row][col] == board[row][col+2] and board[row][col] == board[row][col+3]:
-            print('here2')
-            return True
-
-    else:
-        return False
+def win_checker(i, j):
+         try:
+                  if board[i][j] == board[i-1][j+1] and board[i][j] == board[i-2][j+2] and board[i][j] == board[i-3][j+3]:
+                          return True
+                  elif board[i][j] == board[i-1][j-1] and board[i][j] == board[i-2][j-2] and board[i][j] == board[i-3][j-3]:
+                          return True
+                  elif board[i][j] == board[i][j+1] and board[i][j] == board[i][j+2] and board[i][j] == board[i][j+3]:
+                          return True
+                  elif board[i][j] == board[i-1][j] and board[i][j] == board[i-2][j] and board[i][j] == board[i-3][j]:
+                          return True
+                  else:
+                          return False
+         except:
+                 return False
 
 
 print(win_checker(5, 3))
