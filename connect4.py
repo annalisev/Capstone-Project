ab388720502233b12a0d6ac5111d9f7d71d9e256
annalisev <149485371+annalisev@users.noreply.github.com>
2024-01-16 14:30:41 +0000

    Update connect4.py
    Completed game now with option to play with two players!

--- connect4.py
+++ connect4.py
@@ -22,12 +22,12 @@ def comp_turn():
     return col - 1
 
 
-def get_col():
+def get_col(user):
     # Function which asks which column the user wants to put their counter in then returns this accounting for indexing
     col = 0
     while col == 0:
         inp = int(input(
-            'Which column would you like to place your counter in? Enter a number between 1 and 7: '))
+            f'{user}, which column would you like to place your counter in? Enter a number between 1 and 7: '))
         if height_vals[inp-1] != 6:
             col = inp
         else:
@@ -219,50 +219,123 @@ board = [['0', '0', '0', '0', '0', '0', '0'],
 
 height_vals = [0, 0, 0, 0, 0, 0, 0]
 win_status = False
-user_colour = 0
-current_player = 0
+game_type = 0
 
-# Asking the user which colour they want to be, in a while loop to ensure question gets answered even if user inputs something weird
-while user_colour == 0:
-    print('Would you like to be', colored(
-        'yellow', 'yellow'), 'or', colored('red', 'red'), ' ?')
-    colour_input = input('Please enter either Y or R: ')
-    if colour_input.lower() == 'r':
-        user_colour = colored("R", 'red')
-        comp_colour = colored("Y", 'yellow')
-        print(
-            f'Okay, you are the', colored('Red', 'red'), f' player, you will play with counter {user_colour}!')
-    elif colour_input.lower() == 'y':
-        user_colour = colored("Y", 'yellow')
-        comp_colour = colored("R", 'red')
-        print(
-            'Okay, you are the', colored('Yellow', 'yellow'), f' player, you will play with counter {user_colour}!')
+while game_type != 'T' and game_type != 'C':
+    print('Would you like to play a two-player game or play against the computer?')
+    game_type = input(
+        'Type T for two-player or C to play against the computer: ').upper()
 
+if game_type == 'C':
 
-# Seeing who will start
-print('We will flip a coin to see who will start')
-coin = random.randint(0, 1)
-if coin == 0:
-    print('Tails, the computer will start.')
-elif coin == 1:
-    print('Heads, you will start.')
-    placing_counter(get_col(), board, user_colour)
-    print_board(board)
+    user_colour = 0
+    current_player = 0
+    player_name = input('What is your name?: ')
 
-while win_status == False:
-    win_status, current_player = placing_counter(
-        comp_turn(), board, comp_colour)
-    print('The board is now:')
-    print_board(board)
-    if win_status == True:
-        break
-    win_status, current_player = placing_counter(get_col(), board, user_colour)
-    print('The board is now:')
-    print_board(board)
+    # Asking the user which colour they want to be, in a while loop to ensure question gets answered even if user inputs something weird
+    while user_colour == 0:
+        print('Would you like to be', colored(
+            'yellow', 'yellow'), 'or', colored('red', 'red'), ' ?')
+        colour_input = input('Please enter either Y or R: ')
+        if colour_input.lower() == 'r':
+            user_colour = colored("R", 'red')
+            comp_colour = colored("Y", 'yellow')
+            print(
+                f'Okay, you are the', colored('Red', 'red'), f' player, you will play with counter {user_colour}!')
+        elif colour_input.lower() == 'y':
+            user_colour = colored("Y", 'yellow')
+            comp_colour = colored("R", 'red')
+            print(
+                'Okay, you are the', colored('Yellow', 'yellow'), f' player, you will play with counter {user_colour}!')
 
-print('The game has been won!')
-print(f'Congratulations {current_player} player, you have won!')
-if current_player == user_colour:
-    print('Well Done! You have beaten the computer, how clever!')
+    # Seeing who will start
+    print('We will flip a coin to see who will start')
+    coin = random.randint(0, 1)
+    if coin == 0:
+        print('Tails, the computer will start.')
+    elif coin == 1:
+        print('Heads, you will start.')
+        placing_counter(get_col(player_name), board, user_colour)
+        print_board(board)
+
+    while win_status == False:
+        win_status, current_player = placing_counter(
+            comp_turn(), board, comp_colour)
+        print('The board is now:')
+        print_board(board)
+        if win_status == True:
+            break
+        win_status, current_player = placing_counter(
+            get_col(player_name), board, user_colour)
+        print('The board is now:')
+        print_board(board)
+
+    print('The game has been won!')
+    print(f'Congratulations {current_player} player, you have won!')
+    if current_player == user_colour:
+        print('Well Done! You have beaten the computer, how clever!')
+    else:
+        print('Oh no! The computer has beaten you! Better luck next time!')
+
 else:
-    print('Oh no! The computer has beaten you! Better luck next time!')
+    user1_name = input("Please enter the first player's name: ").capitalize()
+    user2_name = input("Please enter the second player's name: ").capitalize()
+
+    user1_colour = 0
+    user2_colour = 0
+
+    while user1_colour == 0:
+        print(f'{user1_name} would you like to be', colored(
+            'yellow', 'yellow'), 'or', colored('red', 'red'), ' ?')
+        colour_input = input('Please enter either Y or R: ')
+        if colour_input.lower() == 'r':
+            user1_colour = colored("R", 'red')
+            user1_name = colored(user1_name, 'red')
+            user2_colour = colored("Y", 'yellow')
+            user2_name = colored(user2_name, 'yellow')
+            print(
+                f'Okay, {user1_name}, you are the', colored('Red', 'red'), f' player, you will play with counter {user1_colour}!')
+            print(
+                f'{user2_name}, you are the', colored('Yellow', 'yellow'), f' player, you will play with counter {user2_colour}!')
+        elif colour_input.lower() == 'y':
+            user1_colour = colored("Y", 'yellow')
+            user1_name = colored(user1_name, 'yellow')
+            user2_colour = colored("R", 'red')
+            user2_name = colored(user2_name, 'red')
+            print(
+                f'Okay, {user1_name}, you are the', colored('Yellow', 'yellow'), f' player, you will play with counter {user1_colour}!')
+            print(
+                f'{user2_name}, you are the', colored('Red', 'red'), f' player, you will play with counter {user2_colour}!')
+
+    current_player = 0
+
+    # Seeing who will start
+    print('We will flip a coin to see who will start')
+    coin = random.randint(0, 1)
+    if coin == 0:
+        print(f'Tails, {user2_name} will start.')
+        placing_counter(get_col(user2_name), board, user2_colour)
+        print_board(board)
+    elif coin == 1:
+        print(f'Heads, {user1_name} will start.')
+
+    while win_status == False:
+        win_status, current_player = placing_counter(
+            get_col(user1_name), board, user1_colour)
+        print('The board is now:')
+        print_board(board)
+        if win_status == True:
+            break
+        win_status, current_player = placing_counter(
+            get_col(user2_name), board, user2_colour)
+        print('The board is now:')
+        print_board(board)
+
+    print('The game has been won!')
+    print(f'Congratulations {current_player} player, you have won!')
+    if current_player == user1_colour:
+        print(
+            f'Well Done {user1_name}! You have beaten {user2_name}, how clever!')
+    else:
+        print(
+            f'Well Done {user2_name}! You have beaten {user1_name}, how clever!')
