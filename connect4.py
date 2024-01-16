cc4070e022f812a7aa50de115c0893470945f327
annalisev <149485371+annalisev@users.noreply.github.com>
2024-01-16 14:05:11 +0000

    Update connect4.py
    Fully working one player game (playing against the computer)

--- connect4.py
+++ connect4.py
@@ -1,13 +1,18 @@
 import random
+from termcolor import colored
 
 
 def print_board(board):
+    # Function to print the status of the board
     for i in range(6):
-        print(board[i])
+        for j in range(7):
+            print(board[i][j], "  ", end='')
+        print("\n")
     pass
 
 
 def comp_turn():
+    # Function during which the computer takes a turn
     col = 0
     while col == 0:
         num = random.randint(1, 7)
@@ -18,6 +23,7 @@ def comp_turn():
 
 
 def get_col():
+    # Function which asks which column the user wants to put their counter in then returns this accounting for indexing
     col = 0
     while col == 0:
         inp = int(input(
@@ -30,26 +36,180 @@ def get_col():
     return col - 1
 
 
-def win_checker(i, j):
-    if board[i][j] == board[i-1][j+1] and board[i][j] == board[i-2][j+2] and board[i][j] == board[i-3][j+3]:
-        return True
-    elif board[i][j] == board[i-1][j-1] and board[i][j] == board[i-2][j-2] and board[i][j] == board[i-3][j-3]:
-        return True
-    elif board[i][j] == board[i][j+1] and board[i][j] == board[i][j+2] and board[i][j] == board[i][j+3]:
-        return True
-    elif board[i][j] == board[i-1][j] and board[i][j] == board[i-2][j] and board[i][j] == board[i-3][j]:
+board = [['0', '0', '0', '0', '0', '0', '0'],
+         ['0', 'X', '0', '0', '0', '0', '0'],
+         ['0', 'X', '0', 'X', '0', '0', '0'],
+         ['0', 'X', 'X', 'X', 'X', '0', '0'],
+         ['0', 'X', '0', 'X', '0', 'X', '0'],
+         ['X', 'X', '0', 'X', 'X', 'X', 'X']]
+
+
+# Many small functions in order to define win_checker function
+def diagonal_right(row, col):
+    if row <= 2 and col <= 3:
+        if (board[row][col] == board[row+1][col+1]) and (board[row][col] == board[row+2][col+2]) and (board[row][col] == board[row+3][col+3]):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def diagonal_right_shift1(row, col):
+    if row <= 3 and row >= 1 and col <= 4 and col >= 1:
+        if (board[row][col] == board[row-1][col-1]) and (board[row][col] == board[row+2][col+2]) and (board[row][col] == board[row+1][col+1]):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def diagonal_right_shift2(row, col):
+    if row <= 4 and row >= 2 and col <= 5 and col >= 2:
+        if (board[row][col] == board[row-2][col-2]) and (board[row][col] == board[row-1][col-1]) and (board[row][col] == board[row+1][col+1]):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def diagonal_right_shift3(row, col):
+    if row <= 5 and row >= 3 and col >= 3:
+        if (board[row][col] == board[row-2][col-2]) and (board[row][col] == board[row-1][col-1]) and (board[row][col] == board[row-3][col-3]):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def diagonal_left(row, col):
+    if row <= 2 and col >= 3:
+        if (board[row][col] == board[row+1][col-1]) and (board[row][col] == board[row+2][col-2]) and (board[row][col] == board[row+3][col-3]):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def diagonal_left_shift1(row, col):
+    if row <= 3 and row >= 1 and col <= 5 and col >= 2:
+        if (board[row][col] == board[row-1][col+1]) and (board[row][col] == board[row+2][col-2]) and (board[row][col] == board[row+1][col-1]):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def diagonal_left_shift2(row, col):
+    if row <= 4 and row >= 2 and col <= 4 and col >= 1:
+        if (board[row][col] == board[row-2][col+2]) and (board[row][col] == board[row-1][col+1]) and (board[row][col] == board[row+1][col-1]):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def diagonal_left_shift3(row, col):
+    if row <= 5 and row >= 3 and col <= 3:
+        if (board[row][col] == board[row-2][col+2]) and (board[row][col] == board[row-1][col+1]) and (board[row][col] == board[row-3][col+3]):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def diagonal_checker(row, col):
+    return diagonal_right(row, col) or diagonal_right_shift1(row, col) or diagonal_right_shift2(row, col) or diagonal_right_shift3(row, col) or diagonal_left(row, col) or diagonal_left_shift1(row, col) or diagonal_left_shift2(row, col) or diagonal_left_shift3(row, col)
+
+
+def vertical_checker(row, col):
+    if row <= 2:
+        if (board[row][col] == board[row+1][col]) and (board[row][col] == board[row+2][col]) and (board[row][col] == board[row+3][col]):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def checker_right(row, col):
+    if (board[row][col] == board[row][col+1]) and (board[row][col] == board[row][col+2]) and (board[row][col] == board[row][col+3]):
         return True
     else:
         return False
 
 
+def checker_right_shift1(row, col):
+    if (board[row][col] == board[row][col-1]) and (board[row][col] == board[row][col+1]) and (board[row][col] == board[row][col+2]):
+        return True
+    else:
+        return False
+
+
+def checker_left(row, col):
+    if (board[row][col] == board[row][col-1]) and (board[row][col] == board[row][col-2]) and (board[row][col] == board[row][col-3]):
+        return True
+    else:
+        return False
+
+
+def checker_left_shift1(row, col):
+    if (board[row][col] == board[row][col+1]) and (board[row][col] == board[row][col-1]) and (board[row][col] == board[row][col-2]):
+        return True
+    else:
+        return False
+
+
+def horizontal_checker(row, col):
+    if col == 0:
+        return checker_right(row, col)
+
+    elif col == 1:
+        return checker_right(row, col) or checker_right_shift1(row, col)
+
+    elif col == 2:
+        return checker_right(row, col) or checker_right_shift1(
+            row, col) or checker_left_shift1(row, col)
+
+    elif col == 3:
+        return checker_right(row, col) or checker_right_shift1(
+            row, col) or checker_left_shift1(row, col) or checker_left(row, col)
+
+    elif col == 4:
+        return checker_right_shift1(row, col) or checker_left_shift1(
+            row, col) or checker_left(row, col)
+
+    elif col == 5:
+        return checker_left_shift1(row, col) or checker_left(row, col)
+
+    elif col == 6:
+        return checker_left(row, col)
+
+    else:
+        return False
+
+
+def win_checker(row, col):
+    # Function which tells us whether the game is in a win state or not
+    return horizontal_checker(row, col) or vertical_checker(row, col) or diagonal_checker(row, col)
+
+
 def placing_counter(col, board, colour):
+    # Function to update the board when a new counter is placed
     height = height_vals[col]
     board[5-height][col] = colour
     height_vals[col] += 1
-    win_status = win_checker(5-height, col)
+    return win_checker(5-height, col), colour
 
 
+# Initialising the board and other values
 board = [['0', '0', '0', '0', '0', '0', '0'],
          ['0', '0', '0', '0', '0', '0', '0'],
          ['0', '0', '0', '0', '0', '0', '0'],
@@ -60,32 +220,49 @@ board = [['0', '0', '0', '0', '0', '0', '0'],
 height_vals = [0, 0, 0, 0, 0, 0, 0]
 win_status = False
 user_colour = 0
-while user_colour == 0:
-    colour_input = input(
-        'Would you like to be yellow or red? Please enter either Y or R: ')
-    if colour_input.lower() == 'r':
-        user_colour = "R"
-        comp_colour = "Y"
-    elif colour_input.lower() == 'y':
-        user_colour = "Y"
-        comp_colour = "R"
+current_player = 0
 
+# Asking the user which colour they want to be, in a while loop to ensure question gets answered even if user inputs something weird
+while user_colour == 0:
+    print('Would you like to be', colored(
+        'yellow', 'yellow'), 'or', colored('red', 'red'), ' ?')
+    colour_input = input('Please enter either Y or R: ')
+    if colour_input.lower() == 'r':
+        user_colour = colored("R", 'red')
+        comp_colour = colored("Y", 'yellow')
+        print(
+            f'Okay, you are the', colored('Red', 'red'), f' player, you will play with counter {user_colour}!')
+    elif colour_input.lower() == 'y':
+        user_colour = colored("Y", 'yellow')
+        comp_colour = colored("R", 'red')
+        print(
+            'Okay, you are the', colored('Yellow', 'yellow'), f' player, you will play with counter {user_colour}!')
+
+
+# Seeing who will start
 print('We will flip a coin to see who will start')
 coin = random.randint(0, 1)
-'''if coin == 0:
+if coin == 0:
     print('Tails, the computer will start.')
-elif coin == 1:'''
-print('Heads, you will start.')
-placing_counter(get_col(), board, user_colour)
-print_board(board)
+elif coin == 1:
+    print('Heads, you will start.')
+    placing_counter(get_col(), board, user_colour)
+    print_board(board)
 
 while win_status == False:
-    placing_counter(comp_turn(), board, comp_colour)
+    win_status, current_player = placing_counter(
+        comp_turn(), board, comp_colour)
     print('The board is now:')
     print_board(board)
-    placing_counter(get_col(), board, user_colour)
+    if win_status == True:
+        break
+    win_status, current_player = placing_counter(get_col(), board, user_colour)
     print('The board is now:')
     print_board(board)
 
 print('The game has been won!')
-print_board(board)
+print(f'Congratulations {current_player} player, you have won!')
+if current_player == user_colour:
+    print('Well Done! You have beaten the computer, how clever!')
+else:
+    print('Oh no! The computer has beaten you! Better luck next time!')
